cc6350f6df683c2f4ea6bfa32e390044b5547f7c
sonawans <sonawans@PURDLTSONAWANS.ES.AD.ADP.com>
2017-07-31 16:15:17 +0530

    updated action class

--- src/com/adp/bi/selenium/utility/Action.java
+++ src/com/adp/bi/selenium/utility/Action.java
@@ -1,13 +1,15 @@
 package com.adp.bi.selenium.utility;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.adp.bi.tests.TestLocalization;
 import com.adp.main.config.RunTest;
 
 public class Action {
 	static WebDriver driver=RunTest.driver;
-	
+	final static Logger logger = Logger.getLogger(TestLocalization.class.getName());
 		
 	public void getUrl(String url)
 	{
@@ -20,11 +22,13 @@ public class Action {
 	{
 		SeleniumUtils.explicitWaitForElement(element,elementName);
 		element.click();
+		logger.info("Clicked on element "+elementName);
 	}
 	
 	public void type(WebElement element,String textToType,String elementName)
 	{
 		SeleniumUtils.explicitWaitForElement(element,elementName);
 		element.sendKeys(textToType);
+		logger.info("Type in "+elementName);
 	}
 }
